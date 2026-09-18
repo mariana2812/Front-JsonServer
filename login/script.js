8493ebf9c4712f56c16dec1a9af3083f1e07aa44
Mariana Machado <mariana0706.m@gmail.com>
2026-09-18 20:39:14 -0300

    first commit

---
+++ login/script.js
@@ -0,0 +1,41 @@
+// console.log("Login")
+
+// function init() {
+//     const form = document.querySelector("form")
+//     addEventListener ("submit", async (e) => {
+//         e.preventeDefault()
+//         const email = document.querySelector("#email")
+//         const password = document.querySelector("#password")
+//         const paiload = {
+//             email: email.value,
+//             password: password.value
+//         }}
+
+//         const objJson = JSON.stringify(paiload)
+//         const res = await fetch("http://localhost:3000/login", {
+//             method: "POST",
+//             body: objJson,
+//             headers: {
+//                 "Content-Type": "application/json"
+//             }
+//         })
+//     }
+
+//     if (res.ok) {
+//         const data = await res.json()
+//         localStorage.setItem("accessToken", data.accessToken)
+//         localStorage.setItem("idUser", data.user.id)
+//         const resposta = await fetch ('') 
+
+
+
+
+
+
+
+
+
+
+
+
+
